80ade42fec3b3b72501bdfc6d00877fa27bd806f
lykmapipo <lallyelias87@gmail.com>
2019-04-27 22:31:54 +0300

    feat: extend chai with sinon assertions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-export { default as faker } from '@benmaruchu/faker';
-export { default as chai, expect } from 'chai';
-export { default as sinon } from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinon from 'sinon';
+import faker from '@benmaruchu/faker';
+import chai from 'chai';
+
+/* setup chai */
+chai.use(sinonChai);
+const { expect, should } = chai;
+
+export { chai, expect, faker, should, sinon };
